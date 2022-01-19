88bdcd3ba7b26aa449b4def7bed1fa091dc95d4b
Vinicius Inocencio de Souza <vicnicius2009@gmail.com>
2022-01-19 10:23:32 -0300

    adding generateCertificate, dynamo connection error

--- src/functions/hello.ts
+++
@@ -1,9 +0,0 @@
-export const handle = async (event) => {
-  return {
-    statusCode: 201,
-    body: JSON.stringify({ message: "Hello World" }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-};
